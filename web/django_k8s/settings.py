5cd14e97ea40ce88f1cf9e7562f00d1fc840dd65
RajrupDasid <rajrupdasid@outlook.com>
2022-07-03 18:10:51 +0530

    added some unloaded change

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=="1"
 
-ALLOWED_HOSTS = []
+ENV_ALLOWED_HOST=os.environ.get("ENV_ALLOWED_HOST")
 
+ALLOWED_HOSTS = []
+if ENV_ALLOWED_HOST:
+    ALLOWED_HOSTS=[ ENV_ALLOWED_HOST ]
 
 # Application definition
 
